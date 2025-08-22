03879781766f74debbcd663a8b9fa58a4293db3d
Totonyus <mail@totonyus.ovh>
2025-08-22 22:16:01 +0200

    Correction for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ chmod a+x entrypoint.sh
 
 if [ "$DEBUG" == "DEBUG" ]; then
   echo ~~~ Launching DEBUG mode ~~~
-  su "$(id -un $UID)" -c "uvicorn main.py:app --reload --port 8000 --host 0.0.0.0 --reload-include='*.py' --reload-include='frontend/*'"
+  uvicorn main.py:app --reload --port 8000 --host 0.0.0.0 --reload-include='*.py' --reload-include='frontend/*'
 else
-  su "$(id -un $UID)" -c "python3 main.py"
+  python3 main.py
 fi
